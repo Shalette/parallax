d8744f3d620e9c7fa1ec80f91599bb5aed1db24f
Shalette D'Souza <shalettdsouza@gmail.com>
2019-10-12 16:16:17 +0530

    Files added

---
+++ js/index.js
@@ -0,0 +1,13 @@
+init = function(){
+      jarallax = new Jarallax();
+
+      jarallax.addAnimation('.layer8',[{progress: "0%", top:"-20%"}, {progress: "100%", top: "-15%"}]);
+      // jarallax.addAnimation('.layer7',[{progress: "0%", top:"0%"}, {progress: "100%", top: "-10%"}]);
+      // jarallax.addAnimation('.layer6',[{progress: "0%", top:"40%"}, {progress: "100%", top: "40%"}]);
+      // jarallax.addAnimation('.layer5',[{progress: "0%", top:"60%"}, {progress: "100%", top: "-5%"}]);
+      // jarallax.addAnimation('.layer4',[{progress: "0%", top:"20%"}, {progress: "100%", top: "40%"}]);
+      // jarallax.addAnimation('.layer3',[{progress: "0%", top:"-15%"}, {progress: "100%", top: "-5%"}]);
+      // jarallax.addAnimation('.layer2',[{progress: "0%", top:"-15%"}, {progress: "100%", top: "-10%"}]);
+      // jarallax.addAnimation('.layer1',[{progress: "0%", top:"0%"}, {progress: "100%", top: "0%"}]);
+
+    }
