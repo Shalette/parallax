f797994c78c69cfac6295b5e9a01a51fb0d486c2
Shalette D'Souza <shalettdsouza@gmail.com>
2019-10-24 00:55:44 +0530

    Added resize function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
-window.addEventListener('scroll', () => {
-  parent =  document.getElementById('parallax');
-  children = parent.getElementsByTagName('img');
-  for(let i = 0; i < children.length; i++) {
-    children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+$( document ).ready(function(){
+  resize();
+  function resize(){
+    var width = $(window).width();
+    var height = $(window).height();
+    var x = document.getElementsByTagName("img");
+    for (i=0; i<x.length; i++) {
+      $(x[i]).height(height-20);
+    }
+    $('#parallax').height(height);
   }
-})
+
+  $(window).scroll(function()  {
+    parent =  document.getElementById('parallax');
+    children = parent.getElementsByTagName('img');
+    for(let i = 0; i < children.length; i++) {
+      children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+    }
+  });
+
+  $(window).resize(function() {
+    resize();
+  });
+
+});
